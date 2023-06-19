19ca14c0705c2767f97d8e00188db55296a4d479
135-ShintaNuriyatulMahmudiyah <112710815+135-ShintaNuriyatulMahmudiyah@users.noreply.github.com>
2023-06-19 12:06:46 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -37,16 +37,16 @@ with tab2:
                 cap = st.camera_input("Take a picture")
 
                 # Baca frame kamera secara berulang-ulang
-              if cap is not None:
-                    # To read image file buffer with OpenCV:
-                    bytes_data = cap.getvalue()
-                    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-                
-                    # Check the type of cv2_img:
-                    # Should output: <class 'numpy.ndarray'>
-                    st.write(type(cv2_img))
-                
-                    # Check the shape of cv2_img:
-                    # Should output shape: (height, width, channels)
-                    st.write(cv2_img.shape)
+                if cap is not None:
+                        # To read image file buffer with OpenCV:
+                        bytes_data = cap.getvalue()
+                        cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+                    
+                        # Check the type of cv2_img:
+                        # Should output: <class 'numpy.ndarray'>
+                        st.write(type(cv2_img))
+                    
+                        # Check the shape of cv2_img:
+                        # Should output shape: (height, width, channels)
+                        st.write(cv2_img.shape)
 
