e30efd10c67fb82549bb5c266d40717fcac0ce12
135-ShintaNuriyatulMahmudiyah <112710815+135-ShintaNuriyatulMahmudiyah@users.noreply.github.com>
2023-11-22 15:02:18 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
 import re
 import nltk
